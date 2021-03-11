50649725255f1516445825427a577a93ed6967d3
haengun <lucbo49@naver.com>
2021-03-11 11:34:49 +0900

    글쓰기 완료

--- src/main/java/com/cos/blog/web/UserController.java
+++ src/main/java/com/cos/blog/web/UserController.java
@@ -1,5 +1,11 @@
 package com.cos.blog.web;
 
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,18 +13,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cos.blog.config.auth.PrincipalDetails;
+import com.cos.blog.domain.user.User;
+
 @Controller
 public class UserController {
-
-   @GetMapping({"","/"})
-   public String home() {
-      
-      return "index"; //ViewResolver발동
-   }
    
+	//로그인, 로그아웃, 회원가입, 회원정보 변경(AuthController)
+	
+	//
+	
    @GetMapping("/user")
-   public @ResponseBody String hello() { 
+   public @ResponseBody String findAll(@AuthenticationPrincipal PrincipalDetails principalDetails) { 
 	   // @Controller + @ResponseBody = @RestController
+	   //세션 확인 방법(1)
+//	   Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//	   PrincipalDetails principalDetails = (PrincipalDetails)authentication.getPrincipal();
+//	   System.out.println(principalDetails.getUser());
+//	   (2)@AuthenticationPrincipal 어노테이션 걸어주기
+	   System.out.println(principalDetails.getUsername());
       return "User";
    }
 
